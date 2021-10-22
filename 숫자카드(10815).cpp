bb649b7e0606361ad7128015f4b44c07eaa867fc
명진 김 <acws12@nate.com>
2021-10-22 12:34:10 +0900

    Sort

---
+++ 숫자카드(10815).cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+int arr[500001];
+int N, M;
+void Binary(int i, int key) {
+	int mid;
+	int first = 0;
+	int end = i - 1;
+	while (first <= end) {
+		mid = (first + end) / 2;
+		if (arr[mid] == key) {
+			printf("1 ");
+			return;
+		}
+		else if (arr[mid] < key) {
+			first = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+	printf("0 ");
+	return;
+}
+
+void Merge(int list[], int left, int mid, int right) {
+	int i, j, k;
+	int* sortArr = (int*)malloc(sizeof(int) * (right + 1));
+	i = left;
+	j = mid + 1;
+	k = left;
+	while (i <= mid && j <= right) {
+		if (list[i] < list[j]) sortArr[k++] = list[i++];
+		else sortArr[k++] = list[j++];
+	}
+	if (i > mid)
+		for (int x = j; x <= right; x++) sortArr[k++] = list[x];
+	else
+		for (int x = i; x <= mid; x++)sortArr[k++] = list[x];
+	for (int i = left; i <= right; i++) list[i] = sortArr[i];
+	free(sortArr);
+}
+
+void MergeSort(int list[], int left, int right) {
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		MergeSort(list, left, mid);
+		MergeSort(list, mid + 1, right);
+		Merge(list, left, mid, right);
+	}
+}
+
+int main() {
+	scanf("%d",&N);
+	for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+	MergeSort(arr, 0, N-1);
+	scanf("%d", &M);	
+	for (int val, i = 0; i < M; i++) {
+		scanf("%d", &val);
+		Binary(N, val);
+	}
+	return 0;
+}
