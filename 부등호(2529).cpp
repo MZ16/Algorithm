7763db4a8a4bdc84d8270afae341440845de8dd6
명진 김 <acws12@nate.com>
2021-10-23 13:45:59 +0900

    brute force

---
+++ 부등호(2529).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	char s[10];
+	int num[10];
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)num[i] = i;
+	for (int i = 0; i < N; i++)scanf("%c", s[i]);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 1; j < N; j++) {
+			if (s[i] == '>') {
+
+			}
+		}
+	}
+	{
+
+	}
+
+	return 0;
+}
