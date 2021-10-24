f9daa95b4328f6f065f8bbde31a36f0f9983cbda
MZ16 <67396246+MZ16@users.noreply.github.com>
2021-10-24 18:27:24 +0900

    구현

---
+++ 시험 성적(9498).cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+	int N; scanf("%d", &N);
+	if (N >= 90 && N <= 100)printf("A"); else if (N >= 80 && N < 90)printf("B"); else if (N >= 70 && N < 80)printf("C"); else if (N >= 60 && N < 70)printf("D"); else printf("F");
+}
