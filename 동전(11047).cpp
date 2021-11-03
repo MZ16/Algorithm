01295bd748a358452c782abdaf1536ca24cecb18
MZ16 <67396246+MZ16@users.noreply.github.com>
2021-11-03 17:59:19 +0900

    greedy

---
+++ 동전(11047).cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 가장 큰 수부터
+int N, K;
+int arr[11];
+int main() {
+	int ans = 0;
+	scanf("%d %d", &N, &K);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = N - 1; i >= 0; i--) {
+		if (arr[i] <= K) {
+			ans += (K / arr[i]);
+			K %= arr[i];
+		}
+		else if (K == 0)break;
+	}
+
+	printf("%d", ans);
+
+
+}
