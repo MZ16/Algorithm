abb34aedbe9a302ec8c24899740465005d5461f4
명진 김 <acws12@nate.com>
2021-11-08 01:37:32 +0900

    bfs

--- 미로탐색(2178).cpp
+++ 미로탐색(2178).cpp
@@ -1,58 +1,45 @@
-#include <stdio.h>
+#include <iostream>
+#include<algorithm>
 #include <queue>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LEN 101
 using namespace std;
-//dfs는 시간초과, 최단거리는 bfs로
+//dfs는 시간초과, 최단거리는 bfs
+typedef pair<int, int> pii;
+int chk[MAX_LEN][MAX_LEN];
+bool visited[MAX_LEN][MAX_LEN];
+char map[MAX_LEN][MAX_LEN];
+int dyx[4][2] = { {1,0},{-1,0},{0,-1},{0,1} }; // 상하좌우
 int N, M;
-int check[101][101]; // 시작점으로부터의 거리 표시
-int visit[101][101]; // 방문표시
-char map[101][101]; 
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
 
-void bfs(int x, int y)
-{
-	visit[x][y] = 1; 
-
-	queue<pair<int, int> > q; 
-	q.push(make_pair(x, y)); 
-
-	while (!q.empty()) 
-	{
-		int x = q.front().first;
-		int y = q.front().second;
-
-		q.pop(); 
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (0 <= nx && nx < N && 0 <= ny && ny < M) //범위
-			{
-				// 방문x
-				if (map[nx][ny] == '1' && visit[nx][ny] == 0)
-				{
-					check[nx][ny] = check[x][y] + 1;
-					visit[nx][ny] = 1;
-					q.push(make_pair(nx, ny)); 
-				}
-
-			}
-
+void bfs(int y, int x) {
+	visited[y][x] = 1;
+	queue<pii>q;
+	q.push(make_pair(y, x));
+	while (!q.empty()) {
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dyx[i][0];
+			int nx = x + dyx[i][1];
+			if (ny < 0 || nx < 0 || ny >= N || nx >= M||visited[ny][nx]||map[ny][nx]=='0')
+				continue;//미로 범위
+			chk[ny][nx] = chk[y][x] + 1;
+			visited[ny][nx] = 1;
+			q.push(make_pair(ny, nx));				
 		}
 	}
-
 }
 
-int main(void)
-{
+
+
+int main() {
 	scanf("%d %d", &N, &M);
-	for (int i = 0; i < N; i++)
-	{
-		scanf("%s", map[i]);
-	}
+	for (int i = 0; i < N; i++) scanf("%s", &map[i]);
 
 	bfs(0, 0);
-	printf("%d\n", check[N - 1][M - 1] + 1);
+	printf("%d", chk[N - 1][M - 1]+1);
+	return 0;
 }
