1ae5c78c057b51484215f47c7030194a63219b8e
MZ16 <acws12@nate.com>
2022-04-18 21:53:10 +0900

    구현

---
+++ 정수N개의 합(15596).cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+long long sum(int* a, int n) {
+	long long ans = 0;
+	for (int i = 0; i < n; i++) ans += a[i];
+	return ans;
+}
+
+/* 
+* 함수만 작성하면 되는 문제
+* 
+C, C11, C (Clang), C11 (Clang): long long sum(int *a, int n);
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+n: 합을 구해야 하는 정수의 개수
+리턴값: a에 포함되어 있는 정수 n개의 합
+
+C++, C++11, C++14, C++17, C++ (Clang), C++11 (Clang), C++14 (Clang), C++17 (Clang): long long sum(std::vector<int> &a);
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+리턴값: a에 포함되어 있는 정수 n개의 합
+
+*/
