1135beb5ce6e459d257e642c95d2aaecd3009b8a
MZ16 <acws12@nate.com>
2022-05-05 01:58:30 +0900

    SWEA

---
+++ 1545.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = N; i >= 0; i--) {
+		cout << i << " ";
+	}
+
+}
