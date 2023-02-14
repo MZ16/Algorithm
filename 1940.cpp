55fa3fdecac4fceeb9693ec05b6bee74f8a816fa
MZ16 <acws12@nate.com>
2023-02-14 22:37:26 +0900

    .

--- 1940.cpp
+++ 1940.cpp
@@ -15,7 +15,7 @@ int main() {
 	cin >> N;
 	cin >> M;
 	for (int i = 0; i < N; i++) cin >> arr[i];
-	sort(arr, arr+N);
+	//sort(arr, arr+N);
 	if (M > 200000) cout << 0<<"\n";// 고유 번호의 범위가 100,000 임으로 2개의 수를 더했을 때 20만을 초과할 수 없기 때문에 
 									// 코드의 효율성을 위해 추가시켜줌, 해당 문제에선 없어도 맞음
 	else {
