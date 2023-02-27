4386b2b44863ba859eeff3d19827da679185cfe0
MZ16 <acws12@nate.com>
2023-02-28 05:30:18 +0900

    .

--- 4375(2).cpp
+++ 4375(2).cpp
@@ -11,15 +11,15 @@ int main() {
     while (cin >> N) {
         int arr[10000];
         int i = 0;
-        long long num = 1;
-        int cnt = 1;
-        while (num % N != 0) {
-            arr[i++]= (num * 10 + 1) / N;
-            num = (num * 10 + 1) % N;
-            cnt++;
+        long long cnt = 1;
+        int ret = 1;
+        while (cnt % N != 0) {
+            arr[i++]= (cnt * 10 + 1) / N;
+            cnt = (cnt * 10 + 1) % N;
+            ret++;
         }
-        cout << "각 자릿수가 모두 1로 이루어진 가장 작은 수는 " << string(cnt, '1') << "이며, " << "\n";
-        cout << "배수는 : "; for (int j = 0; j < cnt-1; j++) cout << arr[j];
+        cout << "각 자릿수가 모두 1로 이루어진 가장 작은 수는 " << string(ret, '1') << "이며, " << "\n";
+        cout << "배수는 : "; for (int j = 0; j < ret-1; j++) cout << arr[j];
         
     }
     return 0;
