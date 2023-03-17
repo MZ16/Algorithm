059e38e6feb54f5ac873c71d488a04b47984b377
MZ16 <acws12@nate.com>
2023-03-18 04:50:47 +0900

    .

---
+++ 10709.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1 <= H,W <= 100 범위가 작아 반복문으로 해결
+// (i,j) = c 일경우 check용 flag, ret, idx를 초기화시킴
+// 'c'가 아니지만 flag = true면 구름이 이동 가능하며 이동 거리를 구하기위해 idx++
+
+int main() {
+	int N, M;
+	char c[102][102];	
+	int ret[102][102];
+	memset(ret, -1, sizeof(ret));
+	cin >> N >> M;
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {			
+			cin >> c[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		int flag = 0;
+		int idx = 0;
+		for (int j = 0; j < M; j++) {
+			if (c[i][j] == 'c')flag = 1, ret[i][j]=0,idx=0;
+			else if (flag)idx++, ret[i][j] = idx;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << ret[i][j] << " ";
+		}cout << "\n";
+	}
+	return 0;
+}
