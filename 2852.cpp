7396daa43fbb8440fe3dd3aa122af95289cfc40c
MZ16 <acws12@nate.com>
2023-03-21 23:42:50 +0900

    .

---
+++ 2852.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 계산식
+// 1. 상대팀의 득점이 없거나 낮은경우 - (48 - 득점 시간)
+// 2. 득점이 같은경우 - (늦게 득점한 시간 - 먼저 득점한 시간) / 먼저 득점한 팀에 시간 증가
+// flag1,2를 사용 득저
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int N;
+	int arr[2][2][104]; // 팀,득점,시간
+
+	return 0;
+} 
