d287c4bbdc0947839e8224266d33041c2f3cafb5
MZ16 <acws12@nate.com>
2023-03-24 23:48:25 +0900

    .

--- 2852.cpp
+++ 2852.cpp
@@ -1,21 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 계산식
-// 1. 상대팀의 득점이 없거나 낮은경우 - (48 - 득점 시간)
-// 2. 득점이 같은경우 - (늦게 득점한 시간 - 먼저 득점한 시간) / 먼저 득점한 팀에 시간 증가
-// flag1,2를 사용 득저
+// 시간 단위 통일
+// 1. 첫번째 요소가 아닐경우 - Ascore>Bscore or Ascore<Bscore (prev)
+// 2. 현재 스코어 및 시간 저장
+// 3. 마지막 요소인 경우 - max 시간인 48을 처리
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-
+	
 	int N;
-	int arr[2][2][104]; // 팀,득점,시간
-	 
-	for () {
+	int Atime=0, Btime=0;
+	int Ascore=0, Bscore=0;
+	int saveM, saveS;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int team, min, second;
+		scanf("%d %2d:%2d", &team, &min, &second);
+		if (i > 0) { // 1.
+			if (Ascore > Bscore) Atime += ((min * 60) + second) - ((saveM * 60) + saveS);
+			else if(Ascore < Bscore) Btime += ((min * 60) + second) - ((saveM * 60) + saveS);
+		}
 
+		// 2.
+		if (team == 1)Ascore++;
+		else Bscore++;
+		saveM = min; saveS = second;
+		
+		if (i == N - 1) { // 3.
+			if (Ascore > Bscore) Atime = (48 * 60) - (saveM*60+saveS);
+			else if (Ascore < Bscore) Btime = (48 * 60) - (saveM * 60 + saveS);
+		}
 	}
+	cout << Atime / 60 << ":" << Atime % 60<<"\n";
+	cout << Btime / 60 << ":" << Btime % 60 << "\n";
 
 	return 0;
 } 
